252bb335883297fe45f62e04a89a813637bd4f31
Dev-Djam <145383391+Dev-Djam@users.noreply.github.com>
2023-11-06 16:51:02 +0100

    erreur corrigé

--- public/products.php
+++ public/products.php
@@ -1,4 +1,4 @@
 <?php
 
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
-echo $twig->render('products.html.twig', ['bass' => $products]);
+echo $twig->render('products.html.twig', ['products' => $products]);
